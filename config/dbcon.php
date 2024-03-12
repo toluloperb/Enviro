628b0363bd821fab5aea639529ea29292696d5b6
dammy <hemmanueldami@gmail.com>
2024-03-12 14:33:08 +0100

    DBCon Updated

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,14 +1,14 @@
 <?php
 
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "manorrealtor";
-
     // $host = "localhost";
-    // $username = "alphkizf_alphkizf";
-    // $password = "Ipk]dLws-*=q";
-    // $database = "alphkizf_powerbase";
+    // $username = "root";
+    // $password = "";
+    // $database = "manorrealtor";
+
+    $host = "localhost";
+    $username = "manojlmu_manorrealtor";
+    $password = "Trodpen2022*";
+    $database = "manojlmu_manorrealtor";
 
     $con = mysqli_connect($host, $username, $password, $database);
 
