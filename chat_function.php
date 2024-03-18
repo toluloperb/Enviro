e268c9cd95122a124697a6b557c5a3f183f6f6ef
dammy <hemmanueldami@gmail.com>
2024-03-18 12:05:17 +0100

    Updated

--- chat_function.php
+++ chat_function.php
@@ -16,6 +16,30 @@
         {
             $updateSessionId = "UPDATE user_sessions SET session_status = 'New' WHERE session_id = '$session_id'";
             $updateSessionId = mysqli_query($con, $updateSessionId);
+
+            if($updateSessionId)
+            {
+                $to = "admin@manor-realtors.com";
+                $subject = "New Chat Alert!";
+
+                $message = "
+                    <html>
+                        <body>
+                            <p>You have a new Message Waiting from $session_id</p>
+                        </body>
+                    </html>
+                ";
+
+                // Always set content-type when sending HTML email
+                $headers = "MIME-Version: 1.0" . "\r\n";
+                $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+
+                // More headers
+                $headers .= 'From: Manor Live Chat <support@manorrealtorsgroup.com>' . "\r\n";
+                $headers .= 'Cc: support@manorrealtorsgroup.com' . "\r\n";
+                
+                mail($to,$subject,$message,$headers);
+            }
         }
     }
 ?>
