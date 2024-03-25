6d72afb580dd688068bade19ef17e509cb9550f8
dammy <hemmanueldami@gmail.com>
2024-03-25 18:22:06 +0100

    Something to test

--- businesspro/functions/dashboardfunc.php
+++ businesspro/functions/dashboardfunc.php
@@ -73,7 +73,7 @@
             $message = "
                 <html>
                     <body style=\"background: #e8e8e8; text-align: center;\">
-                        <img style=\"width: 60%;\" src=\"https://manorrealtorsgroup.com/businesspro/assets/business pro logo.png\">
+                        <img style=\"width: 60%;\" src=\"https://manorrealtorsgroup.com/businesspro/assets/images/business pro logo.png\">
                         <p style=\"color: #555;\">$messageRun</p>
                     </body>
                 </html>
