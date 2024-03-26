5319ee4fda873c44853a27ac865d915836a6ede6
dammy <hemmanueldami@gmail.com>
2024-03-26 15:49:25 +0100

    Something to test

--- businesspro/functions/dashboardfunc.php
+++ businesspro/functions/dashboardfunc.php
@@ -149,15 +149,14 @@
                         </html>
                     ";
 
-                    // Always set content-type when sending HTML email
-                    // $headers .= "BCC: ". $to . "\r\n";
                     $headers = "MIME-Version: 1.0" . "\r\n";
                     $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 
                     // More headers
                     $headers .= 'From: '. $business_name .' <'. $sender .'@manorrealtorsgroup.com>' . "\r\n";
+                    $headers[] = "Bcc: . ' $to '. ";
 
-                    $mailprocess = mail($to,$subject,$message,$headers);
+                    $mailprocess = mail($to,$subject,$message, implode("\r\n", $headers));
             
                     if($mailprocess)
                     {
