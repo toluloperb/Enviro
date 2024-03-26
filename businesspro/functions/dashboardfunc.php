fb49fd548fbc2bb3a1638d0e2de31de4b598974d
dammy <hemmanueldami@gmail.com>
2024-03-26 15:44:36 +0100

    Something to test

--- businesspro/functions/dashboardfunc.php
+++ businesspro/functions/dashboardfunc.php
@@ -150,7 +150,7 @@
                     ";
 
                     // Always set content-type when sending HTML email
-                    $headers .= "BCC: ". $to . "\r\n";
+                    // $headers .= "BCC: ". $to . "\r\n";
                     $headers = "MIME-Version: 1.0" . "\r\n";
                     $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 
