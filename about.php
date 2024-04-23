c588f7f4c0ea2f6a35d365be696b966cf4a0ae4e
dammy <hemmanueldami@gmail.com>
2024-04-23 07:55:29 +0100

    About us page added

---
+++ about.php
@@ -0,0 +1,52 @@
+<?php
+    include("includes/header.php");
+    include("includes/navbar.php");
+?>
+
+<div class="aboutheader">
+    <h3>About us</h3>
+</div>
+<div class="contents">
+    <div class="background">
+        <div class="recordCover aboutoverlay">
+            <h1>Our Commitment</h1>
+            <h3>Commitment to Real Value</h3>
+            <hr>
+
+            <p>
+            At Manor Realtors, we are in business to create and deliver real value for clients, 
+            shareholders and our own people in a complex world that is constantly changing. <br> <br>
+
+            In response to changing client expectations and market conditions, we assemble teams of experts who 
+            deliver integrated services built on market insight and foresight, sound research and relevant market 
+            knowledge. <br> <br>
+ 
+            We attract, develop and reward the best, and most diverse, people in our industry, challenging them 
+            to develop enduring client relationships built on quality service, collaboration and trust.
+
+            </p>
+        </div>
+    </div>
+
+    <div class="ourpeople">
+        <div class="pics1">
+            <img src="assets/images/Picture1.jpg" alt="">
+        </div>
+        <div class="infos">
+        <h1>Our People</h1>
+            <h3>Commitment to Real Value</h3>
+            <hr>
+            <p>
+            At Manor Realtors, we value the diversity of our people. Our team is made up of dedicated and innovative team members with 
+            different backgrounds and complementary understanding  of the property market, particularly in Sub-Saharan Africa. <br> <br>
+
+            For all our clients, this means access to expert team working  
+            together to achieve a common goal, which is our client’s satisfaction.<br> <br>
+            </p>
+        </div>
+    </div>
+</div>
+
+<?php
+    include("includes/footer.php");
+?>
