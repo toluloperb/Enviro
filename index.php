d5d00499b3a66d10cd0c3ed777e13a29fdd5b9e4
dammy <hemmanueldami@gmail.com>
2024-04-24 05:48:56 +0100

    Animation added to website

--- index.php
+++ index.php
@@ -214,11 +214,11 @@
             <video class="" src="https://cariandbritt.com/wp-content/uploads/2022/05/cari__britt-low-720p.mp4" autoplay="" loop="" muted="muted" playsinline="" controlsList="nodownload"></video>
         </section>
 
-        <section class="explore_prop">
+        <section class="explore_prop sec5 show-animate">
             <div class="title">
-                <h3>Explore Manor Realtors Group</h3>
+                <h3 class="animate">Explore Manor Realtors Group</h3>
             </div>
-            <div class="products">
+            <div class="products animate">
                 <div class="eachproduct" style="background: url(assets/images/home.webp);">
                     <a href=""><div class="overlay"><p>Home</p></div></a>
                 </div>
@@ -241,6 +241,83 @@
 
                 <div class="eachproduct" style="background: url(assets/images/Management.webp);">
                     <a href=""><div class="overlay"><p>Management</p></div></a>
+                </div>
+            </div>
+        </section>
+        <br><br>
+
+        <section class="service sec1 show-animate">
+            <div class="eachs animate">
+                <div class="partImg" style="background: url(assets/images/Picture3.jpg)">
+
+                </div>
+                <div class="partdeets">
+                    <h3>Agency Leasing </h3>
+                    <br>
+                    <p>Earning successful returns on leased commercial property means more than filling space. It begins with a firm grasp of what kind of space the most desirable tenants want and what they will pay for it.  And it is followed by a marketing and agency leasing 
+                        strategy that we will execute to mix the best tenants at the best lease terms – and retain them for lasting value.
+                        <br><br>
+                        Whether you’re an investor, developer, property company or public entity, that’s the way we’ll go to work for you, We’ll bring a strategic, proactive leasing approach to your commercial real estate assets so that you can focus on continuous financial growth.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="service sec2">
+            <div class="eachsreversed animate">
+                <div class="partImg" style="background: url(assets/images/Picture4.png)">
+
+                </div>
+                <div class="partdeets">
+                    <h3>Facility Management </h3>
+                    <br>
+                    <p>In an ideal world, real estate executives manage facilities that operate flawlessly, freeing them to focus on real estate strategies that contribute to broader business and financial goals. 
+                        In reality, many spend much of their work day managing routine facility management and maintenance tasks. 
+                        <br><br>
+                        At Manor Realtors, we know that the ideal can be real. 
+                        <br><br>
+                        Our facility management services typically show clients a 20 percent or more drop in their overall facility operating 
+                        expenses.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="service sec3">
+            <div class="eachs animate">
+                <div class="partImg" style="background: url(assets/images/Picture5.png)">
+
+                </div>
+                <div class="partdeets">
+                    <h3>Tenant Representation / Relocation Management</h3>
+                    <br>
+                    <p>Whether your organization is relocating a single major office or seeking a consistent strategy to acquire 
+                        space and accommodate global expansion, you need a commercial or residential tenant representation firm 
+                        that knows the Nigerian territory.
+                        <br><br>
+                        We serve as your strategic advisor, lowering your real estate costs and occupancy risks while maximizing 
+                        workplace flexibility and productivity. We will analyse your business drivers and occupancy needs, identify 
+                        and evaluate appropriate options, and manage lease negotiations. In fact, we will be your on-call expert 
+                        for all your leasing and lease administration needs. 
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="service sec4">
+            <div class="eachsreversed animate">
+                <div class="partImg" style="background: url(assets/images/Picture6.png)">
+
+                </div>
+                <div class="partdeets">
+                    <h3>Energy and Sustainability</h3>
+                    <br>
+                    <p>Investors and occupiers seem to agree that energy management and sustainability programs benefit their bottom 
+                        line as much as they do our planet. But sustainability goals are easier to announce than to achieve. Leverage 
+                        on our expertise in providing energy management services and sustainability solutions. 
+                        <br><br>
+                        We have one of the deepest in-house teams of environmental experts in the real estate industry
+                    </p>
                 </div>
             </div>
         </section>
@@ -304,10 +381,10 @@
             </div>
         </section>
 
-        <section class="records">
-            <div class="recordCover">
-                <h3>INSPIRED BY SHEER PERFORMANCE? WE’VE GOT YOUR NUMBERS.</h3>
-                <div class="recordContainer">
+        <section class="records sec6">
+            <div class="recordCover ">
+                <h3 class="animate">INSPIRED BY SHEER PERFORMANCE? WE’VE GOT YOUR NUMBERS.</h3>
+                <div class="recordContainer animate">
                     <div class="eachRecord">
                         <p><strong>500+</strong></p>
                         <p>Transaction Completed</p>
